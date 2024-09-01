e3dceffb058a66f5d4d37594c5b215d1e1f7cee0
irakl1ikvinchia <iraklikvinchia8@gmail.com>
2024-09-01 10:10:33 +0300

    commit

---
+++ Day 87/classwork/index.js
@@ -0,0 +1,18 @@
+const num1 = document.getElementById("mydiv")
+const num2 = document.getElementsByTagName("p")
+const num3 = document.getElementsByClassName("p1", "p2", "p3")
+
+console.log(num1.parentElement)
+console.log(num1.children)
+console.log(num1.firstElementChildt)
+console.log(num1.lastElementChild)
+console.log(num1.previousElementSibling)
+console.log(num1.nextElementSibling)
+
+
+// parentElement
+// children
+// firstElementChild 
+// lastElementChild 
+// previousElementSibling 
+// nextElementSibling
